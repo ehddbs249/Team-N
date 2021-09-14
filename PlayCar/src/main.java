51730d88c9d0819f2dfdd01ddcf5a84f6de169fe
ehddbs249 <ehddbs249@gmail.com>
2021-09-15 00:25:17 +0900

    허동윤 수정

--- PlayCar/src/main.java
+++ PlayCar/src/main.java
@@ -3,7 +3,7 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("2021-09-15 AM 12:12 허동윤 수정 !");
+		System.out.println("2021-09-15 AM 12:12 허동윤 수정 3");
 		new MainFrame();
 	}
 
