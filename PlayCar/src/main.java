7fe6d748654acee0e18c39431250394df2711e05
SongMinSun <sms010117@naver.com>
2021-09-16 00:40:19 +0900

    송민선 첫번째 업로드

--- PlayCar/src/main.java
+++ PlayCar/src/main.java
@@ -4,6 +4,9 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("2021-09-15 AM 12:12 Çãµ¿À± ¼öÁ¤ 3");
+		
+		// 203913 ¼Û¹Î¼±
+		System.out.println("2021-09-16 AM 12:29 ¼Û¹Î¼± ¼öÁ¤ ");
 		new MainFrame();
 	}
 
